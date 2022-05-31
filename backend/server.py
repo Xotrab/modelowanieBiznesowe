488c9310d8c53431f12d256b142465177099ec28
Xotrab <mokrylisc@gmail.com>
2022-05-31 17:28:43 +0200

    Renamed the endpoint method and the endpoint route, so it's meaningful now. Added the BPMN image generation and returning.

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, send_file
+import model
 
 app = Flask(__name__)
 
-@app.route("/dupa")
-def dupa():
-    return {"dupcia": [True, False, True]}
+@app.route("/bpmn")
+def generate_BPMN_image():
+    model.generate_BPMN("B2.csv", 0)
+    return send_file('model.png', mimetype='image/png')
 
 if __name__ == "__main__":
     app.run(debug=True)
