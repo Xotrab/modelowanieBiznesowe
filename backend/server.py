25e9d4ad9b8b2e02e258e624fbafd896e65acb89
Xotrab <mokrylisc@gmail.com>
2022-05-31 18:14:34 +0200

    The endpoint now generates the bpmn image based on the uploaded file containing traces. The property name of an uploaded file has to be set to "traces" and the custom header called "Threshold" has to be set with a numeric value in order to pass the event threshold. The uploaded traces are stored in a \traces directory, which can be cleared later on.

--- backend/server.py
+++ backend/server.py
@@ -1,12 +1,33 @@
-from flask import Flask, send_file
+import os
+from flask import Flask, request, send_file
 import model
 
 app = Flask(__name__)
 
-@app.route("/bpmn")
-def generate_BPMN_image():
-    model.generate_BPMN("B2.csv", 0)
-    return send_file('model.png', mimetype='image/png')
+app.config["TRACES_DIRECTORY"] = "./traces"
+app.config["THRESHOLD_HEADER"] = "Threshold"
+
+@app.route("/bpmn", methods=["POST"])
+def bpmn():
+
+    if request.method == "POST":
+
+        if request.files:
+
+            #Note - this requires the frontend upload to set the name property of an input to "traces"
+            traces = request.files["traces"]
+            filepath = os.path.join(app.config["TRACES_DIRECTORY"], traces.filename)
+            traces.save(filepath)
+
+            #Note - this requires the frontend upload to set the custom header called "Threshold" with a numeric value representing the event threshold
+            threshold = int(request.headers[app.config["THRESHOLD_HEADER"]])
+
+            model.generate_BPMN(filepath, threshold)
+            return send_file('model.png', mimetype='image/png')
+        
+        return "No input file provided", 400
+    
+    return "Resource not found", 404
 
 if __name__ == "__main__":
     app.run(debug=True)
