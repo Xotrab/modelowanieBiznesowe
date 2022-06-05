d89b3763b76599d6bcb987ad9d8af15338ce8b3f
Xotrab <mokrylisc@gmail.com>
2022-06-05 11:41:58 +0200

    Added the csv preview logic, which enables the user to define/redefine csv header including cloumn names.

--- frontend/mb-project/src/app/app.module.ts
+++ frontend/mb-project/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatTableModule} from '@angular/material/table';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StartingPageComponent } from './starting-page/starting-page.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { UploadFilePageComponent } from './upload-file-page/upload-file-page.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -12,6 +12,10 @@ import { NgxDropzoneModule } from 'ngx-dropzone';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { CsvPreviewComponent } from './csv-preview/csv-preview.component';
 import { BpmnViewerComponent } from './bpmn-viewer/bpmn-viewer.component';
+import { NgxCsvParserModule } from 'ngx-csv-parser';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
     declarations: [AppComponent, StartingPageComponent, UploadFilePageComponent, NavbarComponent, CsvPreviewComponent, BpmnViewerComponent],
@@ -21,7 +25,13 @@ import { BpmnViewerComponent } from './bpmn-viewer/bpmn-viewer.component';
         NoopAnimationsModule,
         MatButtonModule,
         NgxDropzoneModule,
-        MatSnackBarModule
+        MatSnackBarModule,
+        NgxCsvParserModule,
+        BrowserAnimationsModule,
+        MatTableModule,
+        MatCheckboxModule,
+        FormsModule,
+        MatInputModule
     ],
     providers: [],
     bootstrap: [AppComponent],
--- frontend/mb-project/src/app/csv-preview/csv-preview.component.ts
+++ frontend/mb-project/src/app/csv-preview/csv-preview.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { NgxCsvParser } from 'ngx-csv-parser';
+import { FileHandlerService } from '../file-handler.service';
 
 @Component({
   selector: 'app-csv-preview',
@@ -6,10 +10,131 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./csv-preview.component.scss']
 })
 export class CsvPreviewComponent implements OnInit {
+  public parsedCSV: Array<any>;
+  public isFirstRowHeader: boolean = true;
+  public displayedColumns: Array<string>;
+  public joinedColnames: string;
 
-  constructor() { }
+  public isEditingColnames: boolean = false;
 
-  ngOnInit(): void {
+  constructor(private fileHandler: FileHandlerService, private ngxCsvParser: NgxCsvParser, private snackbar: MatSnackBar, private router: Router) { }
+
+  public ngOnInit(): void {
+    this.parseCSV();
   }
 
+  public parseCSV(): void {
+    this.fileHandler.file$.subscribe(file => {
+      this.ngxCsvParser.parse(file, { header: this.isFirstRowHeader, delimiter: ',' })
+        .pipe().subscribe((result: Array<any>) => {
+          this.parsedCSV = result;
+          this.displayedColumns = this.isFirstRowHeader ?
+            Object.keys(this.parsedCSV[0]) :
+            Array.from({length: this.parsedCSV[0].length}, (_, key) => `${key}`);
+            this.joinedColnames = this.displayedColumns.join(',');
+        }, _ => {
+          console.log("Error occured while parsing the csv file");
+        });
+    });
+  }
+
+  public updateColnames(): void {
+    const newColnames = this.joinedColnames.split(',');
+    const requiredColnames = ['Case ID', 'Activity', 'Start Timestamp'];
+
+    if (newColnames.length !== this.displayedColumns.length) {
+      this.snackbar.open(`Please specify ${this.displayedColumns.length} comma separated columns!`, 'Ok',{
+        duration: 3000
+      });
+
+      return;
+    }
+
+    if (!requiredColnames.every(required => newColnames.includes(required))) {
+      this.snackbar.open('Case ID, Activity and Start Timestamp columns are required to proceed!', 'Ok',{
+        duration: 3000
+      });
+
+      return;
+    }
+
+    if (newColnames.includes('')) {
+      this.snackbar.open('Column name can not be an empty value!', 'Ok',{
+        duration: 3000
+      });
+
+      return;
+    }
+
+    //If the first row was a header, then the parsedCSV includes js objects with colnames as keys, so simply update the keynames
+    if (this.isFirstRowHeader) {
+      for (var index in this.parsedCSV) {
+        var trace = this.parsedCSV[index];
+        var updatedTrace = {};
+
+        this.displayedColumns.forEach((OldColname, index) => {
+          updatedTrace[newColnames[index]] = trace[OldColname];
+        });
+
+        this.parsedCSV[index] = updatedTrace;
+      }
+    }
+    else {
+      //If the first row was not a header, then the parsedCSV includes arrays of values, so we need to create object with colnames as keys
+      for (var index in this.parsedCSV) {
+        var trace = this.parsedCSV[index];
+        var traceObj = {};
+
+        trace.forEach((value: string, index: number) => {
+          traceObj[newColnames[index]] = value;
+        });
+
+        this.parsedCSV[index] = traceObj;
+      }
+    }
+
+    this.displayedColumns = newColnames;
+
+    this.isFirstRowHeader = true;
+
+    //Create the updated file object and set it in the file handler service
+    const csvContent = this.arrayToCSVcontent();
+    const blob = new Blob([csvContent], {type: 'text/plain'});
+
+    const updatedFile = new File([blob],"traces.csv", { type: "text/csv",});
+
+    this.fileHandler.setFile(updatedFile);
+  }
+
+  public nextStep() {
+    const requiredColnames = ['Case ID', 'Activity', 'Start Timestamp'];
+
+    if (!requiredColnames.every(required => this.displayedColumns.includes(required))) {
+      this.snackbar.open('Please define Case ID, Activity and Start Timestamp columns to proceed!', 'Ok',{
+        duration: 3000
+      });
+
+      return;
+    }
+
+    this.router.navigate(['bpmnViewer']);
+  }
+
+  public previousStep() {
+      this.router.navigate(['/uploadFile'])
+  }
+
+  private arrayToCSVcontent(): string {
+    let csvContent = "";
+
+    csvContent += this.joinedColnames + "\n";
+
+    this.parsedCSV.forEach(trace => {
+      const values = Object.values(trace);
+      let joinedTrace = values.join(',');
+      csvContent += joinedTrace + "\n"; 
+    });
+
+    return csvContent;
+  }
 }
